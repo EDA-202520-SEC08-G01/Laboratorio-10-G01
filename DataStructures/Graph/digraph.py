a3d53a4069a3775766883024ae4ff896847e393d
21salazarf <fa.salazar@uniandes.edu.co>
2025-11-14 16:02:41 -0500

    a

--- DataStructures/Graph/digraph.py
+++ DataStructures/Graph/digraph.py
@@ -1,2 +1,17 @@
-from DataStructures.Graph import Graph as g
+from DataStructures.Priority_queue import Priority_queue as pq
+from DataStructures.Map import map_linear_probing as mlp
+import vertex as vtx
 
+
+def new_graph(order):
+    
+    return {
+        "vertices": mlp.new_map(order),
+        "num_edges": 0,
+        
+    }
+
+def insert_vertex(my_graph, key_u, info_u):
+    vertice = vtx.new_vertex(key_u, info_u)
+    mlp.put(my_graph["vertices"], key_u, vertice)
+    
